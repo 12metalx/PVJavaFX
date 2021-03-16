777751e53fe85903d9a67bdc193ac16de292c39e
12metalx <perezozo79@gmail.com>
2021-03-16 13:48:46 -0600

    Cambio en login a undecorated e implementacion de metodos para poder mover

--- src/main/java/com/system/MainApp.java
+++ src/main/java/com/system/MainApp.java
@@ -5,6 +5,9 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.event.EventHandler;
+import javafx.scene.input.MouseEvent;
 
 import java.io.IOException;
 
@@ -12,13 +15,30 @@ public class MainApp extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-
+    private double xOffset = 0;
+    private double yOffset = 0;
 
     @Override
     public void start(Stage primaryStage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader();
         AnchorPane anchorPane = fxmlLoader.load(getClass().getResource("/fxml/Login.fxml"));
         primaryStage.setScene(new Scene(anchorPane));
+        primaryStage.initStyle(StageStyle.UNDECORATED);
+        anchorPane.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                xOffset = event.getSceneX();
+                yOffset = event.getSceneY();
+            }
+        });
+        anchorPane.setOnMouseDragged(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                primaryStage.setX(event.getScreenX() - xOffset);
+                primaryStage.setY(event.getScreenY() - yOffset);
+            }
+        });
         primaryStage.show();
     }
+
 }
