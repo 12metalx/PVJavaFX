9fa2baadaa03320806593258369a1a4d469799e8
12metalx <perezozo79@gmail.com>
2021-03-16 20:59:10 -0600

    Implementacion de metodo para cerrar ventana de login asi como tamanio minimo del dashboard e inicio maximizado

--- src/main/java/com/system/controller/LoginController.java
+++ src/main/java/com/system/controller/LoginController.java
@@ -4,20 +4,29 @@ import com.system.model.dao.UsuarioDAO;
 import com.system.model.dto.UsuarioDTO;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+
+import java.io.IOException;
 import java.net.URL;
 import java.util.Optional;
 import java.util.ResourceBundle;
 
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.fxml.FXML;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
 public class LoginController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
     }
+    @FXML
+    private AnchorPane root;
     @FXML
     private TextField txtUser;
 
@@ -27,7 +36,7 @@ public class LoginController implements Initializable {
     @FXML
     private Button btnLogin;
     @FXML
-    void login(ActionEvent event) {
+    void login(ActionEvent event) throws IOException {
         UsuarioDTO dto = new UsuarioDTO();
         UsuarioDAO dao = new UsuarioDAO();
         dto.getEntidad().setUserName(txtUser.getText());
@@ -37,13 +46,25 @@ public class LoginController implements Initializable {
         alert.setTitle("Confitmation");
         alert.setContentText("Todo ok :)");
         Optional<ButtonType> action = alert.showAndWait();
-        dto = dao.search(dto);
+       /* dto = dao.search(dto);
         if(dto != null){
             System.out.println("Todo ok :)");
         }
         else{
             System.out.println("Errros en passs, pero todo bien xd");
-        }
+        }*/
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/Dashboard.fxml"));
+        BorderPane borderPane = loader.load();
+        Stage stage = new Stage();
+        stage.setScene(new Scene(borderPane));
+        stage.setMaximized(true);
+        stage.setMinWidth(1280);
+        stage.setMinHeight(720);
+        com.system.util.Methods.closeEffect(this.root);
+        stage.show();
+
+
     }
     @FXML
     void close(MouseEvent event) {
